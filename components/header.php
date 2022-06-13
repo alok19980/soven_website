0634275e68e628e97d5e9e0fcb9f93eb73bafd8f
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-06-13 06:42:59 +0530

    My First commit

---
+++ components/header.php
@@ -0,0 +1,122 @@
+<header class="header default">
+      <div class="topbar">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="d-block d-md-flex align-items-center text-center">
+                <div class="me-4 d-inline-block py-1">
+                  <a href="#"><i class="far fa-envelope me-2 fa-flip-horizontal text-primary"></i>admin@sovendeveloper.com</a>
+                </div>
+                <div class="me-auto d-inline-block py-1">
+                  <a><i class="fas fa-map-marker-alt text-primary me-2"></i>2055 Limestone Rd STE 200-C, Wilmington, DE 19808, United States</a>
+                </div>
+                <div class="d-inline-block py-1">
+                  <ul class="list-unstyled">
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">FAQ</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav class="navbar bg-white navbar-static-top navbar-expand-lg">
+        <div class="container-fluid">
+          <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target=".navbar-collapse"><i class="fas fa-align-left"></i></button>
+          <a class="navbar-brand" href="index.php">
+            <img class="img-fluid" src="images/logo.png" alt="logo">
+          </a>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#" id="navbarDropdown" role="button"  aria-haspopup="true" aria-expanded="false">Home</a>
+               
+              </li>
+              <li class="dropdown nav-item">
+                <a href="properties.html" class="nav-link" data-bs-toggle="dropdown">Services</a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="contact.html">Contact<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="pricing.html">Pricing<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="testimonials.html">Testimonials</a></li>
+                    <li><a class="dropdown-item" href="blog.html">Blog 01<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="blog-02.html">Blog 02<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="blog-detail.html">Blog Detail<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="elements.html">Elements<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="faq.html">Faq<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="coming-soon.html">Coming Soon<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="error-404.html">404 Error<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="privacy-policy.html">Privacy Policy<i class="fas fa-arrow-right"></i></a></li>
+                    <li><a class="dropdown-item" href="terms-and-conditions.html">Terms & Conditions<i class="fas fa-arrow-right"></i></a></li>
+                  </ul>
+              </li>
+              <li class="nav-item">
+                <a href="properties.html" class="nav-link" data-bs-toggle="dropdown">About Us</a>
+                  
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link" data-bs-toggle="dropdown">Contact Us</a>
+                
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link" >Portfolio</a>
+              </li>
+               <li class="dropdown nav-item mega-menu">
+                <a href="#" class="nav-link" data-bs-toggle="dropdown">Our Products</a>
+                <ul class="dropdown-menu megamenu">
+                  <li>
+                    <div class="row">
+                      <div class="col-sm-6 col-lg-3">
+                        <h6 class="mb-3 nav-title">Menu Title </h6>
+                        <ul class="list-unstyled mt-lg-3">
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                        </ul>
+                      </div>
+                      <div class="col-sm-6 col-lg-3">
+                        <h6 class="mb-3 nav-title">Menu Title</h6>
+                        <ul class="list-unstyled mt-lg-3">
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                        </ul>
+                      </div>
+                      <div class="col-sm-6 col-lg-3">
+                        <h6 class="mb-3 nav-title">Menu Title</h6>
+                        <ul class="list-unstyled mt-lg-3">
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                        </ul>
+                      </div>
+                      <div class="col-sm-6 col-lg-3">
+                        <h6 class="mb-3 nav-title">Menu Title</h6>
+                        <ul class="list-unstyled mt-lg-3">
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                          <li><a href="#">Menu item </a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div class="d-none d-sm-flex ms-auto me-5 me-lg-0 pe-4 pe-lg-0">
+            <ul class="nav ms-1 ms-lg-2 align-self-center">
+              <li class="contact-number nav-item pe-4 ">
+                <a class="fw-bold" href="#"><i class="fab fa-whatsapp pe-2"></i>+(44) 7596087222</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
