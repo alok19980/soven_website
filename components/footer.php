9153c117e9d4345c4bce8f909fa4711546e6c2d5
Alok Singh Chauhan <alokchauhan.kalpi@gmail.com>
2022-06-14 07:15:47 +0530

    "Added Live Chat Plugin"

--- components/footer.php
+++ components/footer.php
@@ -91,3 +91,16 @@
         </div>
       </div>
     </footer>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62a7e6c37b967b1179946024/1g5ftqrt1';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
